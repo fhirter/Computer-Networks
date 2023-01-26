3bef0bb5afe7e2f9e88d9f9d7171a6a35c18d8aa
fabian hirter <mail@fabianhirter.ch>
2023-01-26 11:17:24 +0100

    add error detection exercise solutions

--- Exercises/ErrorDetection_Solution.py
+++ Exercises/ErrorDetection_Solution.py
@@ -11,3 +11,10 @@ class ErrorDetection:
         # check if occurences count is odd or even and compare to parity bit
         is_valid = (one_count % 2) == int(parity_bit)
         return is_valid
+
+    def get_parity(self, number):
+        binary_string = bin(number)
+        one_count = binary_string.count('1')
+        one_count += 1 # increment to get even parity
+        parity = (one_count % 2)
+        return parity
--- Exercises/Test_ErrorDetection_Solution.py
+++ Exercises/Test_ErrorDetection_Solution.py
@@ -5,16 +5,24 @@ from ErrorDetection_Solution import ErrorDetection
 class TestErrerDetection(unittest.TestCase):
     def test_parity_check_valid(self):
         error_detection = ErrorDetection()
-        number_with_parity = 0b101001011
+        number_with_parity = 0b101101010
         parity = error_detection.is_parity_valid(number_with_parity)
 
         self.assertEqual(parity, True)
 
     def test_parity_check_invalid(self):
         error_detection = ErrorDetection()
-        number_with_parity = 0b001001011
+        number_with_parity = 0b001101010
         parity = error_detection.is_parity_valid(number_with_parity)
         self.assertEqual(parity, False)
+
+    def test_parity_creation(self):
+        error_detection = ErrorDetection()
+        number = 0b10110101
+        expected_parity = 0
+        actual_parity = error_detection.get_parity(number)
+
+        self.assertEqual(expected_parity, actual_parity)
 
 if __name__ == '__main__':
     unittest.main()
