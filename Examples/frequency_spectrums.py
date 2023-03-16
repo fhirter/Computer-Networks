f26dd44390cf0823737d6d63ebdbb184a22cf116
Fabian Hirter <mail@fabianhirter.ch>
2023-03-16 13:41:07 +0100

    add noise, set levels with dB

--- Examples/frequency_spectrums.py
+++ Examples/frequency_spectrums.py
@@ -1,18 +1,31 @@
+from control import db2mag
 from scipy.signal import iirfilter, lfilter
 
 from plot_helpers import *
 from signal_helpers import *
 
+# relevant parameters
+critical_frequencies = [5, 10000]
+signal_level_db = 0
+noise_level_db = -100
+
+
 # signal parameters
 signal_type = "square"  # one of: square, sine, sawtooth, noise
-signal_frequency =1000
+signal_frequency =100
 
 # filter parameters
-critical_frequencies = [50, 5000]
 filter_order = 2
 
 sampling_frequency = 50000
+
+# noise
+noise = create_signal("noise", signal_frequency, sampling_frequency)
 signal = create_signal(signal_type, signal_frequency, sampling_frequency)
+
+signal_multiplier = db2mag(signal_level_db)
+noise_multiplier = db2mag(noise_level_db)
+signal = signal_multiplier*signal+noise_multiplier*noise
 
 # filter signal
 filter_type = "bandpass"
