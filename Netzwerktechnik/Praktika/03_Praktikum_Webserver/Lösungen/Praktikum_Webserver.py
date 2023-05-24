f6ebba1ee68e8ecfa5b5a7f4bb9abfcf10ac2382
Fabian Hirter <mail@fabianhirter.ch>
2023-05-24 10:18:10 +0200

    update webserver solution

--- Netzwerktechnik/Praktika/03_Praktikum_Webserver/Lösungen/Praktikum_Webserver.py
+++ Netzwerktechnik/Praktika/03_Praktikum_Webserver/Lösungen/Praktikum_Webserver.py
@@ -6,18 +6,33 @@ serverPort = 8080
 
 class SimpleWebserver(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
+
+        status = 200
+        if self.path == "/info":
+            body = "This is a info page"
+        elif self.path == "/":
+            body = "This is the default page."
+        else:
+            body = "404 not found!"
+            status = 404
+
+        self.send_response(status)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+        self.wfile.write(bytes(f'''
+            <html><head><title>https://pythonbasics.org</title></head>
+            <body>
+                <p>{body}</p>
+            </body></html>
+        ''', "utf-8"))
+
     def do_POST(self):
+        content_length = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_length)
+        print(post_body)
         self.send_response(200)
         self.end_headers()
-        print("post")
 
 
 if __name__ == "__main__":
