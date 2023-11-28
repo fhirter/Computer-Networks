b46686e5cf4ada98055f44702cbe90409665a8b3
fabian hirter <mail@fabianhirter.ch>
2023-11-28 14:34:20 +0100

    add solutions

---
+++ Netzwerktechnik/Praktika/04_REST_API/Lösungen/README.md
@@ -0,0 +1,14 @@
+# README
+
+## install dependencies
+```shell
+npm install
+```
+
+## run server
+```shell
+node index.js
+```
+
+## watch swagger docs
+[http://localhost:8080/api-docs/#/](http://localhost:8080/api-docs/#/)
--- Netzwerktechnik/Praktika/04_REST_API/Lösungen/index.js
+++ Netzwerktechnik/Praktika/04_REST_API/Lösungen/index.js
@@ -1,16 +1,69 @@
-const express = require('express')
-const app = express()
-const port = 8080
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Returns a list of users
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ *   post:
+ *     summary: create a new user
+ *     responses:
+ *       201:
+ *         description: A new user has been created
+ */
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const user = {
-  name: "John Doe"
+    name: "John Doe"
 }
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'My API',
+            version: '1.0.0',
+            description: 'A simple Express API',
+        },
+    },
+    apis: ['./index.js'], // paths to files containing OpenAPI definitions
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
 app.get('/users', (req, res) => {
-  res.status(200);
-  res.send(user);
+    res.status(200);
+    res.send([user]);
+})
+
+app.post('/users/{user}', (req, res) => {
+    res.status(201);
+    res.send();
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
