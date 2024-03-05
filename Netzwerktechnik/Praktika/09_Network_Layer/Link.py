2c5917ad8ee70fc0d7373330c67c89bb9f26bae7
Fabian Hirter <mail@fabianhirter.ch>
2024-03-05 16:42:27 +0100

    add ipv6 message

--- Netzwerktechnik/Praktika/09_Network_Layer/Link.py
+++ Netzwerktechnik/Praktika/09_Network_Layer/Link.py
@@ -1,9 +1,9 @@
-from Message import Message
+from IPv6Message import IPv6Message
 
 
 class Link:
     def __init__(self, callback):
         self._callback = callback
 
-    def send(self, message: Message):
+    def send(self, message: IPv6Message):
         self._callback(message)
--- Netzwerktechnik/Praktika/09_Network_Layer/TestLink.py
+++ Netzwerktechnik/Praktika/09_Network_Layer/TestLink.py
@@ -1,13 +1,13 @@
 import unittest
 
 from Link import Link
-from Message import Message
+from IPv6Message import IPv6Message
 
 
 class TestLink(unittest.TestCase):
     def test_message_forwarding(self):
         destination = 0b10111
-        message = Message(destination, 'Hello World!')
+        message = IPv6Message(destination, 'Hello World!')
 
         def callback(msg):
             self.assertEqual(message.value, msg.value)
