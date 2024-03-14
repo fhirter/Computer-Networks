e2145323230c2147a549ce0c50b145c0751a6a79
fabian hirter <mail@fabianhirter.ch>
2024-03-14 11:05:54 +0100

    check if port is present

--- Netzwerktechnik/Praktika/09_Network_Layer/Router.py
+++ Netzwerktechnik/Praktika/09_Network_Layer/Router.py
@@ -17,13 +17,20 @@ class Router:
 
     def __forward(self, message: IPv6Packet):
         destination = message.destination_address
-        if destination in self.forwarding_table:
-            port_id = self.forwarding_table[destination]
-            port = self.ports[port_id]
-            port.send_out(message)
-            print(f"Forwarded Packet to {destination} at Router {self.router_id}")
-        else:
+        if destination not in self.forwarding_table:
             print(f"No forwarding entry for {destination}")
+            return
+
+        port_id = self.forwarding_table[destination]
+
+        if port_id > len(self.ports):
+            print(f"Port {port_id} not present!")
+            return
+
+        port = self.ports[port_id]
+        print(f"Forwarded Packet to {destination} at Router {self.router_id}")
+        port.send_out(message)
+
 
     def attach(self, port_id, other_router, other_port_id):
         self.ports[port_id].connect(other_router.ports[other_port_id])
